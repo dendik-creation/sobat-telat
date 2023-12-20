5c17bc8bdbdb0cd2680ea257618c8bebeac84a0f
dendik-creation <setiawandendik0205@gmail.com>
2023-12-20 20:16:27 +0700

    Published for Public

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,19 +20,5 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-
-        // User::whereBetween('id', [434, 435])->update([
-        //     // 'nis' => 'admin1',
-        //     'password' => bcrypt('12345'),
-        // ]);
-
-        // User::create([
-        //     'nis' => 'admin2',
-        //     'nama' => 'Sang Hyang Prabu Admin II',
-        //     'nisn' => 'admin',
-        //     'gender' => 'L',
-        //     'kelas_id' => 37,
-        //     'password' => bcrypt('12345'),
-        // ]);
     }
 }
